6fa55d936a65eaf0d2634ec93f621408cd6f1c92
resaend <46780128+resaend@users.noreply.github.com>
2020-11-12 12:06:46 +0700

    Add files via upload

---
+++ shopee.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Bot Shopee V1.2 ©res\n";
+sleep(1);
+echo "[+]Login akun shopee\n";
+sleep(2);
+echo "[+]Berhasil login\n";
+sleep(1);
+echo "[+]Masukan Link Flash Sale : ";
+$link = trim(fgets(STDIN));
+sleep(3);
+echo "[+]Menunggu Flash Sale Dimulai...\n";
+sleep(5);
+echo "=> Nama Barang : Poco X3 NFC (6+64GB) Snapdragon 732G Flash Sale Spesial 11 11\n=> Status : Berhasil \n"
+?>
